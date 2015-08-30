6e4433f2da0c721558163f067e04122e2cc5350e
DeborahPerez <perezdeboraha@gmail.com>
2015-08-30 17:20:41 -0400

    Added description to documentation

--- count_seq.sh
+++ count_seq.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-#@USAGE: bash count_seq.sh FASTA_FILE
+# USAGE: bash count_seq.sh FASTA_FILE
+# DESCRIPTION: A simple script to count the number of sequences in a 
+FASTA file 
 grep -c ">" $1
